41da4bc4c25333a73cf92c49ec077467fd406019
sebbengon <sebastianbengtsson@aol.com>
2026-03-30 17:29:23 +0200

    Massborttagning

--- vue.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  // if the app is supposed to run on Github Pages in a subfolder, use the following config:
-  publicPath: process.env.NODE_ENV === "production" ? "/HowToPlay/" : "/"
-  // publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
-};
